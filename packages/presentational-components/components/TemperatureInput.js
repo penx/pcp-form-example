a213436a5b9f40973a208a9be005f964ecba0f6f
Alasdair McLeay <alasdair.mcleay@me.com>
2017-11-15 14:47:03 +0000

    Add example TemperatureInput component from https://reactjs.org/docs/lifting-state-up.html

---
+++ packages/presentational-components/components/TemperatureInput.js
@@ -0,0 +1,33 @@
+// from https://reactjs.org/docs/lifting-state-up.html
+
+import React from 'react';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput
--- packages/presentational-components/stories/index.js
+++ packages/presentational-components/stories/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import TemperatureInput from '../components/TemperatureInput';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('TemperatureInput', module)
+  .add('Celsius', () => <TemperatureInput scale="c" />)
+  .add('Fahrenheit', () => <TemperatureInput scale="f" />)
