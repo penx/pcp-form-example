bd94b2e5822f0695ae3b99cc808ccf91c147cda9
Alasdair McLeay <alasdair.mcleay@me.com>
2017-11-15 14:55:38 +0000

    Add Calculator and BoilingVerdict components from https://reactjs.org/docs/lifting-state-up.html

--- packages/presentational-components/stories/index.js
+++ packages/presentational-components/stories/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import TemperatureInput from '../components/TemperatureInput';
+import Calculator from '../components/Calculator';
 
 storiesOf('TemperatureInput', module)
   .add('Celsius', () => <TemperatureInput scale="c" />)
   .add('Fahrenheit', () => <TemperatureInput scale="f" />)
+
+
+storiesOf('Calculator', module)
+  .add('Celsius', () => <Calculator />)
